eea77bd076eb3e9935204cbbd0901ecc4f5ccad7
Htet Oo Wai Yan <sc3p73r@outlook.com>
2024-09-23 11:14:44 +0630

    Day-2 update

--- Day-2/ec2.tf
+++ Day-2/ec2.tf
@@ -2,11 +2,6 @@ resource "aws_instance" "web01" {
   ami = "ami-01811d4912b4ccb26"
   instance_type = "t2.micro"
    tags = {
-     Name = "Web Server"
+     Name = "welcom to vtmm"
    }
-}
-
-resource "local_file" "name" {
-  filename = ""
-  content = ""
 }
